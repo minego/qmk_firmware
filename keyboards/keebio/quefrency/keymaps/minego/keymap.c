47eb44f75b937b680bd1d780a243cfc6602e9f54
Micah N Gorrell <micah.gorrell@venafi.com>
2020-04-13 13:17:32 -0600

    I'm on a mac now so... Ugh

--- keyboards/keebio/quefrency/keymaps/minego/keymap.c
+++ keyboards/keebio/quefrency/keymaps/minego/keymap.c
@@ -16,7 +16,7 @@ enum custom_keycodes {
     tapped and like modifiers when held.
 */
 #define CTRLESC LCTL_T(KC_ESC)
-#define TP_LEFT RGUI_T(KC_LEFT)
+#define TP_LEFT RALT_T(KC_LEFT)
 #define TP_UP   RSFT_T(KC_UP)
 #define TP_RGHT RCTL_T(KC_RGHT)
 #define TP_DOWN LT(FN_1, KC_DOWN)
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
     CTRLESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TP_UP,   TP_UP,   \
-    KC_LCTL, KC_LALT, KC_LGUI, TT(FN_1),SH_SPC,           SH_SPC,  SH_SPC,  KC_RALT, TP_LEFT, TP_LEFT, TP_DOWN, TP_RGHT
+    KC_LCTL, KC_LALT, KC_LGUI, TT(FN_1),SH_SPC,           SH_SPC,  SH_SPC,  KC_RGUI, TP_LEFT, TP_LEFT, TP_DOWN, TP_RGHT
   ),
 
   [FN_1] = LAYOUT(
